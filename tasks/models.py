db51dc9f65f751b5eb07498c00d75aa346eff460
Caroline-Wacera <carolinewacera4@gmail.com>
2026-01-04 16:29:27 +0300

    Create tasks app, register Task model, and configure settings

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User  # built-in user model
 
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # link task to a user
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.DateField()         # date of the task
+    time = models.TimeField()         # time of the task
+    completed = models.BooleanField(default=False)  # checker
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {'Done' if self.completed else 'Pending'}"
 # Create your models here.
